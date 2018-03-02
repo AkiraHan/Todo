0538e8893b1cd06011900e8f631e53ab063bdfd9
AkiraHan <33615984+AkiraHan@users.noreply.github.com>
2018-03-02 17:07:38 -0500

    Update index.js

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ var config = require('./config.json');
 module.exports = {
 
     getDbConnectionString(){
-        return "mongodb://" + config.uname + ":" + config.pwd + "@ds249418.mlab.com:49418/nodetodosample";
+        return "mongodb://" + config.uname + ":" + config.pwd + "@*********************"; // this is my mongodb URL, cannot show it here.
     }
 
 };
